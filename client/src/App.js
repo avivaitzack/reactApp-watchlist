831bda1a914ae5e7349c4ef3f7a682e2ac046a50
aviva itzack <aviva8999@gmail.com>
2022-11-11 04:46:11 +0200

    adding files and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React, {  Route,  Link, Routes, } from "react-router-dom";
 import LogIn from "./Components/LogIn";
 import Home from "./Components/home";
+import NavBar from './Components/Nav';
+import Footer from './Components/Footer';
+import Favorites from "./Components/Favorites";
+import Register from "./Components/Register";
 
 export default function App() {
   return (
     <div> 
       <Link to={'/Home'}>home </Link><br/>
-      <Link to={'/login'}> log-in </Link>
+      <Link to={'/login'}> log-in </Link><br/>
+      <Link to={'/register'}> register </Link><br/>
+      <Link to={'/favorites'}> favorites </Link><br/>
+      <Link to={'/footer'}> footer </Link><br/>
+      <Link to={'/navbar'}> navbar </Link>
       <Routes>
         <Route path="/Home" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/navbar" element={<NavBar />} />
       </Routes>
     </div>
   );
